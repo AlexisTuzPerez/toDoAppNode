2189dab365432afdfa7d1d7a601164fb1d2cc8a4
alexistuz <g.alexistuz@gmail.com>
2025-05-12 12:28:45 -0600

    idk

--- backend/server.js
+++ backend/server.js
@@ -18,8 +18,10 @@ mongoose.connect(uri)
   .then(() => console.log('MongoDB connected'))
   .catch(err => console.error('MongoDB connection error:', err));
 
+console.log("BASE_URL", BASE_URL)
+
 app.use(cors({
-  origin: process.env.BASE_URL,
+  origin: 'http://localhost:5173',
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization', 'Cookie'],
